c051b0436e15a3095a0941c77083559ea25b1845
Bento Benack <bentobenack@gmail.com>
2022-05-02 14:47:48 -0400

    Create 1491_average_salary_excluding_the_minimum_and_maximum_salary.py

---
+++ solutions/1491_average_salary_excluding_the_minimum_and_maximum_salary.py
@@ -0,0 +1,7 @@
+from functools import reduce
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.remove(max(salary))
+        salary.remove(min(salary))
+        
+        return sum(salary)/len(salary)
